50c518b8252436b5d870e4799eeaca9232ba2785
Harish V <harishv207@gmail.com>
2017-03-05 16:21:53 +0800

    Initial

---
+++ js/myjs.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	var showPhotos = function(photos){
+		$.get( "../photography.json", function( data ) {
+			console.log(data);
+		});
+
+		var photos = [];
+		$.ajax({
+			url: "https://api.imgur.com/3/account/harishv/album/lMXdg",
+			headers: {"Authorization": "Client-ID 15c2f7712f1356c"},
+			success: function(data) {
+				console.log(data);
+				var images = data.data.images;
+				
+				for (var i = 0; i < images.length; i++) {
+					photos.push({
+						src: images[i].link,
+						width: images[i].width,
+						height: images[i].height
+					});
+				}
+				console.log(photos);
+
+				$('.image-container').empty().justifiedImages({
+					images : photos,
+					rowHeight: 200,
+					maxRowHeight: 350,
+					thumbnailPath: function(photo, width, height){
+						var purl = photo.url_s;
+						//				if( photo.url_n && (width > photo.width_s * 1.2 || height > photo.height_s * 1.2) ) purl = photo.url_n;
+						//				if( photo.url_m && (width > photo.width_n * 1.2 || height > photo.height_n * 1.2) ) purl = photo.url_m;
+						//				if( photo.url_z && (width > photo.width_m * 1.2 || height > photo.height_m * 1.2) ) purl = photo.url_z;
+						//				if( photo.url_l && (width > photo.width_z * 1.2 || height > photo.height_z * 1.2) ) purl = photo.url_l;
+						return purl;
+					},
+					getSize: function(photo){
+						return {width: photo.width, height: photo.height};
+					},
+					margin: 3
+				});
+			},
+			type: 'GET'
+		});
+		//		var photos = [{
+		//			src: "http://placehold.it/350x150",
+		//			width_s: 350,
+		//			height_s: 150
+		//		},{
+		//			src: "http://placehold.it/550x650",
+		//			width_s: 550,
+		//			height_s: 650
+		//		}];
+	}
+	showPhotos();
+});
